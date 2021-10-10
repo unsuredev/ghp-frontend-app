8266c8a3278dd4a2d784277259ba77c1a396a5eb
jamal <jamaluddin.mondal@mysmitch.com>
2021-10-10 14:55:29 +0530

    base url change due to restart ec2

--- src/Common/constant.ts
+++ src/Common/constant.ts
@@ -1,7 +1,8 @@
 export  const BASE_URL =
     process.env.BASE_URL || 
 //    "ec2-15-206-88-174.ap-south-1.compute.amazonaws.com:3001/v1/"
-    "http://ec2-15-206-88-174.ap-south-1.compute.amazonaws.com:3001/v1/"
+    "http://ec2-15-207-100-228.ap-south-1.compute.amazonaws.com:3001/v1/"
+    
     //"https://jamanenterprise.herokuapp.com/"
     
     //"http://localhost:3001/v1/"
